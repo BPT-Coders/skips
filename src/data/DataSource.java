96cc565bd3847baf7c6fe0d44eb72555f9a866f5
Andrey <bgmir213@gmail.com>
2016-03-15 14:45:14 +0400

    Рефакторинг DataSource.java: 	- Инициализация настроек вынесена в отдельный класс 	- Удалён дублирующийся код

--- src/data/DataSource.java
+++ src/data/DataSource.java
@@ -1,188 +1,74 @@
 package data;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.*;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.InvalidPropertiesFormatException;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DataSource {
-    HashMap<String, String> settings;
-    String userName;
-    String password;
-    String url;
-    
-    Connection conn = null;
-    Statement stmt = null;
-    ResultSet rs = null;
+public class DataSource {    
+    Connection conn;
+    Statement stmt;
+
     public DataSource() {
+        Settings settings = new Settings();
+        String userName = settings.getUserDB();
+        String password = settings.getPassDB();
+        String url = settings.getURLDB();
+        openCon(userName, password, url);
+    }
+    
+    final void openCon(String user, String pass, String url){
         try {
-            initSettings();
-        } catch (IOException ex) {
+            try {
+                Class.forName ("com.mysql.jdbc.Driver").newInstance();
+            } catch (InstantiationException ex) {
+                Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
             Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, ex);
         }
-        userName = settings.get("userName");
-        password = settings.get("password");
-        url = settings.get("url");
-    }
-    void initSettings() throws IOException{
-        String filePath = ".\\etc\\properties.ini";
-        FileInputStream fileInputStream = null;
-        //FileOutputStream fileOutputStream = null;
-        Properties properties = new Properties();
-        settings = new HashMap<String, String>();
         try {
-            fileInputStream = new FileInputStream(filePath);
-            properties.load(fileInputStream);
-            Enumeration enumeration = properties.keys();
-            while (enumeration.hasMoreElements()){
-                String key = enumeration.nextElement().toString();
-                settings.put(key, properties.getProperty(key));
-            }
-            //fileOutputStream = new FileOutputStream(filePath);
-            //properties.store(fileOutputStream, "mod");
-        } catch (FileNotFoundException e) {
-            new File("property.ini").createNewFile();
-            System.out.println("createNewFile");
-            e.getMessage();
-        } catch (InvalidPropertiesFormatException e) {
-            e.getMessage();
-        } catch (IOException e) {
-            e.getMessage();
-        } finally {
-            //fileOutputStream.close();
-            fileInputStream.close();
-        }
-    }
-    public boolean test(){
-        try
-        {       
-            Class.forName ("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection (url, userName, password);
-            return true;
-        }
-        catch (Exception ex)
-        {
-            return false;
-        }
-        finally {
-            closeCon();
+            conn = DriverManager.getConnection (url, user, pass);
+            stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public String[][] getTab(String t){
+    public String[][] getTab(String q){
         String[][] arr = null;
         try
         {       
-            Class.forName ("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection (url, userName, password);   
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("select * from " + t + ";");
+            ResultSet rs = stmt.executeQuery(q);
             rs.last();
-            int m = rs.getRow();
-            int n = rs.getMetaData().getColumnCount();
-            rs.beforeFirst();
+            int m = rs.getRow();//Количество строк
+            int n = rs.getMetaData().getColumnCount();//Количество столбцов
             arr = new String[m][n];
+            rs.beforeFirst();
             int i = 0;
             while (rs.next()){
                 for (int j = 1; j <= n; j++){
-                    arr[i][--j] = rs.getString(++j);
+                    arr[i++][--j] = rs.getString(++j);
                 }
-                i++;
             }          
         }
         catch (Exception ex)
         {
             ex.printStackTrace();
         }
-        finally {closeCon();}
         return arr;
-    }
-    
-    public String[][] getRow(String q){
-        String[][] arr = null;
-        try
-        {       
-            Class.forName ("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection (url, userName, password);   
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(q);
-            rs.last();
-            int m = rs.getRow();
-            int n = rs.getMetaData().getColumnCount();
-            rs.beforeFirst();
-            arr = new String[m][n];
-            int i = 0;
-            while (rs.next()){
-                for (int j = 1; j <= n; j++){
-                    arr[i][--j] = rs.getString(++j);
-                    System.out.println(i);
-                }
-                i++;
-            }          
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-        finally {closeCon();}
-        return arr;
-    }
+    }  
 
     public void exe(String q) throws SQLException{
         try
-        {       
-            Class.forName ("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection (url, userName, password);
-            stmt = conn.createStatement();          
+        {                
             stmt.executeUpdate(q);
         }
         catch (Exception ex)
         {
             ex.printStackTrace();
-        }
-        finally
-        {
-            closeCon();
-        }       
+        }    
     }
-    
-    void closeCon(){
-        if (conn != null)
-            {
-                try
-                {
-                    conn.close ();
-                }
-                catch (Exception ex) { }
-            }
-    }
-    public boolean isAvailableShedule(String teacherLoadsId, String sheduleDate){
-        boolean result = false;
-        try
-        {       
-            Class.forName ("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection (url, userName, password);   
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT id from shedule where teacherLoadId in (" + teacherLoadsId + ") and date ='" + sheduleDate + "';");
-            if (rs.next()){
-                result = true;
-            }
-            else{
-                result = false;
-            }
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-        finally {closeCon();}
-        return result;
-    }
+       
 }
